3728af834207b2007f8f7eb33fed63fe59136411
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2022-02-15 22:19:50 -0800

    Update findCIMC.ps1

--- findCIMC.ps1
+++ findCIMC.ps1
@@ -1,9 +1,9 @@
 # This script connects to the provided IP's and makes a Redfish API call and prints the CIMC product,vendor Info.
 # Need to add Error checks
 
-$subnet   = Read-Host "Please enter the subnet id in format x.y.z: [198.19.216]"
-$start_ip = Read-Host "Please enter the start IP last octet: [163]"
-$end_ip   = Read-Host "Please enter the end IP last octet: [167]"
+$subnet   = Read-Host "Please enter the subnet id in format [198.19.216]: "
+$start_ip = Read-Host "Please enter the start IP last octet [163]: "
+$end_ip   = Read-Host "Please enter the end IP last octet [167]: "
 
 $start_ip..$end_ip | ForEach-Object {
     $targetIp = "$subnet.$_"
@@ -12,7 +12,22 @@ $start_ip..$end_ip | ForEach-Object {
     Write-Host " "
     $response = ""
     $uri = "https://$targetIp/redfish/v1"
-    $response = Invoke-RestMethod -SkipCertificateCheck -Method 'GET' -Uri $uri -TimeoutSec 2 | select Product,Vendor
-    Write-Host $response | ConvertTo-Json
-    Write-Host " "
+    try {
+        $response = Invoke-WebRequest -SkipCertificateCheck -Method 'GET' -Uri $uri -TimeoutSec 2
+        if ($response.StatusCode -eq 200) {
+            $responseStatusCode = $response.StatusCode
+            Write-Host "Response Status Code: $responseStatusCode"
+            $product = ($response.Content | ConvertFrom-Json).Product
+            $vendor = ($response.Content | ConvertFrom-Json).Vendor
+            Write-Host "Product Id: $product, Company: $vendor"
+            Write-Host " "
+        }
+        else {
+            Write-Host "Response Status Code: $response.StatusCode"
+            Write-Host "Not a Cisco UCS Server"
+        }
+    } catch {
+        Write-Host $_.Exception.Response.StatusDescription
+        Write-Host $_.Exception.Response.StatusCode.Value__
+    }
 }
