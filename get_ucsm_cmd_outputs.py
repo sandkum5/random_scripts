9f32742dee8041c218a6f18b78b214f378486c92
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2023-12-14 22:30:08 -0800

    Update get_ucsm_cmd_outputs.py

--- get_ucsm_cmd_outputs.py
+++ get_ucsm_cmd_outputs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
     Script to gather UCSM(NXOS, IOM, VIC(Adapter)) Command outputs for troubleshooting
-    Uses ucsm_data.yml file as input. 
+    Uses ucsm_data.yml file as input.
     Update the ucsm_data.yml file with the UCSM IP, username, and commands to collect.
     Note: The command needs to be entered in sequence along with "exit" commands to exit from scopes(NXOS, IOM, VIC level scopes).
 """
@@ -10,6 +10,7 @@ from datetime import datetime
 import time
 import yaml
 import getpass
+import socket
 
 # Ref: https://github.com/paramiko/paramiko/issues/2238
 def transport_factory(*args, **kwargs):
@@ -39,6 +40,26 @@ def print_output(shell, log_file):
             time.sleep(1)  # Adjust delay if needed
         print(output)
 
+def check_server(address, port):
+    # Create a TCP socket
+    timeout = 10 #timeout in seconds
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(timeout)
+    print (f"Attempting to connect to {address} on port {port}!")
+    try:
+        s.connect((address, port))
+        reachable = True
+        #print ("Connected to %s on port %s" % (address, port))
+        print(f"Connected to {address} on port {port}!")
+        return reachable
+    except socket.error as error:
+        reachable = False
+        #print ("Connection to %s on port %s failed: %s")
+        print(f"Connection to {address} on port {port} failed: {error}!")
+        return reachable
+    finally:
+        s.close()
+
 
 if __name__ == '__main__':
 
@@ -52,33 +73,39 @@ if __name__ == '__main__':
     password = getpass.getpass(prompt='Enter UCSM Password: ')
     log_file = f'{datetime.now().strftime("%Y%m%d-%H%M%S")}_{data["log_file"]}'
 
-    # SSH Connection
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # Verify TCP connectivity to UCSM on port 443
+    tcp_conn = check_server(address=hostname, port=443)
 
-    try:
-        ssh.connect(hostname, username=username, password=password, transport_factory=transport_factory)
-        shell = ssh.invoke_shell()
+    if tcp_conn:
+        # SSH Connection
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        # Commands to Execute in UCSM
-        commands = data["commands"]
+        try:
+            ssh.connect(hostname, username=username, password=password, transport_factory=transport_factory)
+            shell = ssh.invoke_shell()
 
-        # Send commands and write output to log_file
-        for command in commands:
-            print(f"Command: {command}")
-            shell.send(f"{command}\n")
-            time.sleep(1)
-            print_output(shell, log_file)
-            time.sleep(.5)
+            # Commands to Execute in UCSM
+            commands = data["commands"]
 
-        # Close Shell and SSH session
-        shell.close()
-        ssh.close()
+            # Send commands and write output to log_file
+            for command in commands:
+                print(f"Command: {command}")
+                shell.send(f"{command}\n")
+                time.sleep(1)
+                print_output(shell, log_file)
+                time.sleep(.5)
 
-    except paramiko.AuthenticationException as auth_exception:
-        print(f"Authentication failed: {auth_exception}")
-    except paramiko.SSHException as ssh_exception:
-        print(f"SSH connection failed: {ssh_exception}")
-    except Exception as e:
-        print(f"Error: {e}")
-        ssh.close()
+            # Close Shell and SSH session
+            shell.close()
+            ssh.close()
+
+        except paramiko.AuthenticationException as auth_exception:
+            print(f"Authentication failed: {auth_exception}")
+        except paramiko.SSHException as ssh_exception:
+            print(f"SSH connection failed: {ssh_exception}")
+        except Exception as e:
+            print(f"Error: {e}")
+            ssh.close()
+    else:
+        print("UCSM Not reachable!")
